f1d851687a69d918e977405aa466057a79e5efea
adityabhargav8 <109024296+adityabhargav8@users.noreply.github.com>
2022-07-21 22:26:48 +0530

    Add files via upload

---
+++ asssingment4.c
@@ -0,0 +1,119 @@
+// // solution 1
+// #include <stdio.h>
+// int main()
+// {
+//     for (int i = 1; i <= 5; i++)
+//     {
+//         printf("MySirG\n");
+//     }
+
+//     return 0;
+// }
+
+// // solution 2
+// #include <stdio.h>
+// int main()
+// {
+//     for (int i = 1; i <= 10; i++)
+//     {
+//         printf("%d\n", i);
+//     }
+
+//     return 0;
+// }
+
+// // solution 3
+// #include <stdio.h>
+// int main ()
+// {
+//     for (int i =10 ; i>=1; i--)
+//     {
+//         printf("%d\n", i);
+//     }
+
+// return 0;
+// }
+
+// // solution 4
+// #include <stdio.h>
+// int main()
+// {
+//     for (int i = 1; i <= 19; i +=2)
+//     {
+//         printf("%d\n", i);
+//     }
+
+//     return 0;
+// }
+
+// // solution 5
+// #include <stdio.h>
+// int main ()
+// {
+// for (int i =19; i >=1; i-=2)
+// {
+//      printf("%d\n", i);
+// }
+
+// return 0;
+// }
+
+// // solution 6
+// #include <stdio.h>
+// int main()
+// {
+//     for (int i = 2; i <= 20; i += 2)
+//     {
+//         printf("%d\n", i);
+//     }
+
+//     return 0;
+// }
+
+// // solution 7
+// #include <stdio.h>
+// int main()
+// {
+//     for (int i=20; i>1; i -= 2)
+//     {
+//         printf("%d\n", i);
+//     }
+
+//     return 0;
+// }
+
+// //solution 8
+// #include <stdio.h>
+// int main ()
+// {
+// for (int i = 1; i <=10; i++)
+// {
+//      printf("%d\n", i*i);
+// }
+
+// return 0;
+// }
+
+// // solution 9
+// #include <stdio.h>
+// int main()
+// {
+//     for (int i = 1; i <= 10; i++)
+//     {
+//         printf("%d\n", i * i * i);
+//     }
+
+//     return 0;
+// }
+
+// // solution 10
+// #include <stdio.h>
+// int main()
+// {
+//     for (int i = 1; i <= 10; i++)
+//     {
+//         printf("5 x %d = %d\n",i,5*i);
+//     }
+
+//     return 0;
+// }
