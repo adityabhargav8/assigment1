92f800c7779867ca9ef43a67486ff1064944a285
adityabhargav8 <109024296+adityabhargav8@users.noreply.github.com>
2022-07-21 22:31:43 +0530

    Add files via upload

---
+++ assigment1.c
@@ -0,0 +1,151 @@
+// SOLUTION 1.
+
+// #include <stdio.h>
+//     int
+//     main()
+// {
+//     pritnf("Hello students \n");
+//     return 0;
+// }
+
+// SOLUTION 2.
+// #include <stdio.h>
+//     int
+//     main()
+// {
+//     printf("Hello \n students");
+//     return 0;
+// }
+
+// SOLUTION 3.
+// #include <stdio.h>
+//     int
+//     main()
+// {
+//     printf(" \"MySirG\" ");
+//     return 0;
+// }
+
+// SOLUTION 4.
+// #include <stdio.h>
+//     int
+//     main()
+// {
+//     int r;
+//     printf("Enter radius of a circle\n");
+//     scanf("%d", &r);
+//     printf("Area of circle is %f having radius %d ", (3.14 * r * r), r);
+//     return 0;
+// }
+
+// SOLUTION 5.
+// #include <stdio.h>
+// #include <string.h>
+//     int
+//     main()
+// {
+//     char a[20];
+//     printf("enter your name\n");
+//     scanf("%s", a);
+//     printf("length of string is %d", strlen(a));
+//     return 0;
+// }
+
+// After understanding 12th question
+// #include <stdio.h>
+//     int
+//     main()
+// {
+//     int x;
+//     char a[30];
+//     printf("Enter a string\n");
+//     scanf("%s", a);
+//     x = printf("%s", a);
+//     printf("\nlength of string is: %d", x);
+//     return 0;
+// }
+
+// SOLUTION 6.
+// #include <stdio.h>
+//     int
+//     main()
+// {
+//     char a[20];
+//     printf("enter your name\n");
+//     scanf("%s", a);
+//     printf("Hello ,\"%s\"", a);
+//     return 0;
+// }
+
+// SOLUTION 7.
+
+// #include <stdio.h>
+//     int
+//     main()
+// {
+//     printf("%%d");
+//     return 0;
+// }
+
+// SOLUTION 8.
+
+// #include <stdio.h>
+//     int
+//     main()
+// {
+//     printf("\\n");
+//     return 0;
+// }
+
+// SOLUTION 9.
+// #include <stdio.h>
+//     int
+//     main()
+// {
+//     printf("\\");
+//     return 0;
+// }
+
+// SOLUTION 10.
+// #include <stdio.h>
+//     int
+//     main()
+// {
+//     int d, m, y;
+//     printf("Enter DATE : \b");
+//     scanf("%d%d%d", &d, &m, &y);
+//     if (d <= 31 && m <= 12 && y > 1999)
+//     {
+//         printf(" Day- %d, Month- %d , Year- %d", d, m, y);
+//     }
+//     else
+//     {
+//         printf("enter a valid date\n");
+//     }
+
+//     return 0;
+// }
+
+// SOLUTION 11.
+// #include <stdio.h>
+//     int
+//     main()
+// {
+//     int hh, mm;
+//     printf("Enter time : \b");
+//     scanf("%d%d", &hh, &mm);
+//     printf("Time is %d hours and %d minutes\n", hh, mm);
+//     return 0;
+// }
+
+// SOLUTION 12.
+// #include <stdio.h>
+//     int
+//     main()
+// {
+//     int x = printf("ineuron");
+//     printf("%d", x);
+//     return 0;
+// }
+
+// OUTPUT IS : ineuron7
